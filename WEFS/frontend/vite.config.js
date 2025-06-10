35326ebbc2e864cb5b3b15c86b88a191698e8268
kunal <kunal.212034105@vcet.edu.in>
2025-06-10 16:38:56 +0530

    vercel json added

--- WEFS/frontend/vite.config.js
+++ WEFS/frontend/vite.config.js
@@ -13,13 +13,20 @@ export default defineConfig({
   },
   server: {
     host: "localhost",
-    port: process.env.FRONTEND_PORT,
+    port: process.env.FRONTEND_PORT || 3000,
     proxy: {
       "^/api(/|(\\?.*)?$)": {
-        target: `http://127.0.0.1:${process.env.BACKEND_PORT}`,
+        target: `http://127.0.0.1:${process.env.BACKEND_PORT || 8000}`,
         changeOrigin: true,
         secure: false,
       },
     },
+  },
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(
+      process.env.NODE_ENV === "production"
+        ? ""
+        : `http://localhost:${process.env.BACKEND_PORT || 8000}`
+    ),
   },
 });
