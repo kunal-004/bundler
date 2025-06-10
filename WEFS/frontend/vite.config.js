512d8e24c6cb82521a8e926ca111e1b778e4ca6c
kunal <kunal.212034105@vcet.edu.in>
2025-06-10 12:34:59 +0530

    made config changes

--- WEFS/frontend/vite.config.js
+++ WEFS/frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // Optional, helps with proper routing
+  base: "./", // Optional, helps with proper routing
   plugins: [react()],
   resolve: {
     preserveSymlinks: true,
